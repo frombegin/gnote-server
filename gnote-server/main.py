e789df85fa49c72a9eeb72210042ddca7438cf4b
frombegin <frombegin@gmail.com>
2013-12-16 22:20:07 +0800

    more requirements

--- gnote-server/main.py
+++ gnote-server/main.py
@@ -4,12 +4,23 @@
 import tornado.ioloop
 import tornado.web
 
+from threadpool import ThreadPool
+
+
+class Application(tornado.web.Application):
+    
+    def __init__(self, handlers=None, default_host="", transforms=None,
+                 wsgi=False, **settings):
+        super(Application, self).__init__(handlers, default_host, transforms, wsgi, **settings)
+        self.threadpool = ThreadPool(10)
+
 class MainHandler(tornado.web.RequestHandler):
     
     def get(self):
         self.write("Hello, world")
 
-application = tornado.web.Application([
+#application = tornado.web.Application([
+application = Application([
     (r"/", MainHandler),
 ])
 
