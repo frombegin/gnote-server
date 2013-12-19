f678158fcb9647adde24ee2f7e201e69555180d9
frombegin <frombegin@gmail.com>
2013-12-19 22:31:44 +0800

    urls

--- gnote-server/main.py
+++ gnote-server/main.py
@@ -4,25 +4,53 @@
 import tornado.ioloop
 import tornado.web
 
-from threadpool import ThreadPool
-
-
-class Application(tornado.web.Application):
-    
-    def __init__(self, handlers=None, default_host="", transforms=None,
-                 wsgi=False, **settings):
-        super(Application, self).__init__(handlers, default_host, transforms, wsgi, **settings)
-        self.threadpool = ThreadPool(10)
+import binascii
 
 class MainHandler(tornado.web.RequestHandler):
     
     def get(self):
         self.write("Hello, world")
 
-#application = tornado.web.Application([
-application = Application([
+    def _writeDict(self, d):
+        for key, value in d.iteritems():
+            self.write("%s -> %s\n" % (key, binascii.hexlify(str(value))))
+
+    
+
+    def post(self):
+        self._writeDict(self.request.headers)
+        self.write("\n")
+
+        self.write("-"*40 + "\n")
+
+        self._writeDict(self.request.files)
+        self.write("\n")
+        #self.write(self.request.body)
+
+application = tornado.web.Application([
     (r"/", MainHandler),
-])
+], debug = True)
+
+[
+"user/register",
+"user/login",
+"user/logout",
+"user/groups",
+
+"lobby/findgroup",
+"lobby/tags",
+"lobby/finduser",
+"lobby/timeline",
+
+"group/create",
+"group/join",
+"group/invite",
+"group/approval",
+"group/post",
+"group/timeline",
+
+]
+
 
 if __name__ == "__main__":
     application.listen(8888)
